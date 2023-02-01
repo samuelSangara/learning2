477d3a3da9338ce0add8851617d5b926f396d6da
samuelSangara <124121329+samuelSangara@users.noreply.github.com>
2023-02-01 19:42:28 +0200

    new function

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const gender=[,1,2,3,,4,5,6,6,6,6,5,5,5,]
 
 
-function countOccurence(array,searcheElement){
-    let gen=0;
-    for(let element of gender)
-    if (element === searcheElement)
-    gen ++;
-    return gen;
+
+const browser=[1,2,3,4,5,6,7,8,9];
+
+function move(array, index, offset){
+    const position=index+  offset;
+    if(array.length=== 0 || position < 0){
+        console.error('urgent come and change it.')
+    }
+    const back=[...browser];
+    const element=browser.splice(index, 1)[0]
+    back.splice(position, 0, element)
+    return back;
 }
-const count= countOccurence(gender,6);
 
-console.log(count)
-
+const hire= move(browser,0,15)
+console.log(hire)
