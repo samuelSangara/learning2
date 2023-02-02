2b259055262d8cede85a850b2872631f273a0841
samuelSangara <124121329+samuelSangara@users.noreply.github.com>
2023-02-02 21:09:40 +0200

    this is addition of h4

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
+const blame=[1,2,3,5,5,];
+function except(array, excluded){
+    const slim=[];
+    for(let element of blame)
+    if (!excluded.includes(element))
+    slim.push(element);
+    return slim;
+}
+const thin= except(blame,[2])
+console.log(thin)
 
-
-let goodCredit=true;
-let rateIncome=false;
-
-let eligibleForLoan=goodCredit || rateIncome;
-
-const applicationRefused= !eligibleForLoan;
-
-console.log(eligibleForLoan);
-console.log('applicationRefused' , applicationRefused);
